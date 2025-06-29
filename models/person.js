100b85e3ad9b223c15b7d04c4cc19af99ef97856
Tiziano Jahn <jahntiziano@gmail.com>
2025-06-29 13:11:06 +0200

    added eslint and fixed it

--- models/person.js
+++ models/person.js
@@ -1,18 +1,19 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false)
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
-console.log("connecting to", url);
+console.log('connecting to', url)
 mongoose
   .connect(url)
   .then((result) => {
-    console.log("connected to MongoDB");
+    console.log(result)
+    console.log('connected to MongoDB')
   })
   .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
   id: Number,
@@ -26,48 +27,48 @@ const personSchema = new mongoose.Schema({
     minLength: 8,
     validate: {
       validator: function (v) {
-        return  /\d{2,3}-\d+/.test(v);
+        return /\d{2,3}-\d+/.test(v)
       },
       message: (props) => `${props.value} is not a valid phone number!`,
     },
-    required: [true, "User phone number required"],
+    required: [true, 'User phone number required'],
   },
-});
+})
 
-const Person = mongoose.model("Person", personSchema);
+const Person = mongoose.model('Person', personSchema)
 
 const generateId = () => {
-  const randomId = Math.floor(Math.random() * 100000000);
-  return randomId;
-};
+  const randomId = Math.floor(Math.random() * 100000000)
+  return randomId
+}
 
 if (process.argv.length === 3)
   Person.find({}).then((result) => {
-    console.log("phonebook:");
+    console.log('phonebook:')
     result.forEach((person) => {
-      console.log(`${person.name} ${person.number}`);
-    });
-    mongoose.connection.close();
-  });
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
 
 if (process.argv.length === 5) {
-  const name = process.argv[3];
-  const number = process.argv[4];
+  const name = process.argv[3]
+  const number = process.argv[4]
 
-  const person = new Person({ id: generateId(), number, name });
+  const person = new Person({ id: generateId(), number, name })
 
   person.save().then(() => {
-    console.log(`Added ${name} number ${number} to phonebook`);
-    mongoose.connection.close();
-  });
+    console.log(`Added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
 }
 
-personSchema.set("toJSON", {
+personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model('Person', personSchema)
